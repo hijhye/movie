19d0e238e5afff2216d91e20d9590137d6399f56
hijhye <yesyuji17@gmail.com>
2025-11-26 17:01:50 +0900

    first

---
+++ js/custom.js
@@ -0,0 +1,144 @@
+//movie
+let APIKey = "10f974a2eddec8dc4c2a71f15af77591";
+
+let currentPage = 1;
+let currentList = "now_playing";
+let currentKeyword = "";
+let totalPage = "";
+
+async function movie(lists, page = 1) {
+  currentPage = page;
+  currentList = lists;
+  currentKeyword = "";
+  let url = `https://api.themoviedb.org/3/movie/${lists}?api_key=${APIKey}&language=ko-KR&page=${currentPage}`;
+
+  let response = await fetch(url);
+  let data = await response.json();
+  let movieList = data.results;
+  totalPage = data.total_pages;
+  console.log(movieList);
+  console.log(totalPage);
+
+  render(movieList);
+}
+let wrap = document.querySelector(".wrap");
+let list = document.querySelector(".list");
+let movieBoard = document.querySelector("#movieBoard");
+let bottomBtn = document.querySelector("#bottomBtn");
+
+function render(movieList) {
+  let result = "";
+
+  movieList.forEach((movie, rank) => {
+    // console.log(i + 1);
+    // console.log(movie.backdrop_path);
+    // console.log(movie.title);
+    // console.log(movie.release_date);
+    globalRank = (currentPage - 1) * 20 + (rank + 1);
+    let imgSrc = movie.backdrop_path
+      ? `https://media.themoviedb.org/t/p/w355_and_h200_multi_faces${movie.backdrop_path}`
+      : "https://placehold.co/355x200/555/FFF?text=No..";
+    result += `
+          <li class="movie">
+            <div class="imgWrap">
+              <div class="num">${globalRank}</div>
+              <img src="${imgSrc}" alt="${movie.title}">
+            </div>
+            <h3>${movie.title}</h3>
+            <p>${movie.release_date}</p>
+          </li>
+    `;
+  });
+
+  wrap.style.marginTop = "-100vh";
+  list.style.display = "flex";
+  bottomBtn.style.display = "flex";
+  movieBoard.innerHTML = result;
+}
+
+let homeBtn = document.querySelector("#home");
+homeBtn.addEventListener("click", function () {
+  totalPage = 1;
+  wrap.style.marginTop = "0vh";
+});
+
+let searchInput = document.querySelector(".searchWrap #user");
+let searchBtn = document.querySelector("#searchBtn");
+
+searchBtn.addEventListener("click", () => {
+  currentList = "";
+  currentPage = 1;
+  keyword = searchInput.value.trim();
+  currentKeyword = keyword;
+  //   console.log(keyword);
+
+  if (currentKeyword == "") {
+    alert("검색어를 입력하세요~");
+    return;
+  }
+
+  searchInput.value = "";
+  //api 호출
+  search(currentKeyword, currentPage);
+});
+
+async function search(currentKeyword, currentPage) {
+  let url = `https://api.themoviedb.org/3/search/movie?query=${currentKeyword}&api_key=${APIKey}&language=ko-KR&page=${currentPage}`;
+
+  let response = await fetch(url);
+  let data = await response.json();
+
+  console.log(data);
+  let movieList = data.results;
+  totalPage = data.total_pages;
+  //   console.log(movieList);.
+  render(movieList);
+}
+
+//더보기
+let more = document.querySelector("#nextBtn");
+
+more.addEventListener("click", moreview);
+function moreview() {
+  if (currentPage == totalPage) {
+    alert("더이상 영화 없음");
+    return;
+  } else {
+    currentPage++;
+  }
+  console.log(currentList, currentPage, currentKeyword);
+  if (currentKeyword == "") {
+    movie(currentList, currentPage);
+  } else if (currentList == "") {
+    search(currentKeyword, currentPage);
+  }
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
+
+let prev = document.querySelector("#prevBtn");
+
+prev.addEventListener("click", preview);
+function preview() {
+  if (currentPage == 1) {
+    alert("더이상 앞으로 갈 수 없음");
+    return;
+  } else {
+    currentPage--;
+  }
+
+  console.log(currentList, currentPage, currentKeyword);
+  if (currentKeyword == "") {
+    movie(currentList, currentPage);
+  } else if (currentList == "") {
+    search(currentKeyword, currentPage);
+  }
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+}
